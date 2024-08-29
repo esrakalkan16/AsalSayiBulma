ab1c6d6e85553a78e06010c6cfbc1c4024cbe8ec
Esra <147397832+esrakalkan16@users.noreply.github.com>
2024-08-29 17:49:33 +0300

    Asal Sayı Bulma

---
+++ AsalSayiBulma/AsalSayiBulma/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsalSayiBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asal sayı bulma programı
+            int sayi;
+
+            Console.Write("{0}\n-> Bir sayı giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 0)
+            {
+                for (int i = 2; i < sayi; i++)
+                {
+                    if(sayi%i== 0)
+                    {
+                        Console.WriteLine("Sayı asal sayıdeğildir.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sayı asal sayıdır.");
+                        break;
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("0 ve negatif sayılarda asal sayı aranmaz!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
